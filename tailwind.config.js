12e375e9834a8012d0630f6c676944c21c3d385e
Pintér József <ptrj1990@gmail.com>
2024-01-21 09:34:38 +0100

    chore: initialize project, install dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#CC3B3B",
+        yellow: "#FFCB05",
+        lightYellow: "#FFCB053D",
+        blue: "#2E6EB5",
+        grayOne: "#6E6E6E",
+        grayTwo: "#EBEDED",
+        grayThree: "#A6ADB4",
+        grayFour: "#A5A7A7",
+        grayFive: "#676D74",
+        graySix: "#2E6EB53C",
+        dark: "#202124",
+      },
+    },
+  },
+  plugins: [],
+};
